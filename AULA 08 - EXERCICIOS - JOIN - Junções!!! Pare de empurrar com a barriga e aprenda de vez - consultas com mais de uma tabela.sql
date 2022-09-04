49e46e3869157ecba5ce4346a47e6a73b38ee130
Vinícius Margonar <103221467+ViniciusMargonar@users.noreply.github.com>
2022-09-03 22:00:07 -0300

    Add files via upload

---
+++ AULA 08 - EXERCICIOS - JOIN - Junções!!! Pare de empurrar com a barriga e aprenda de vez - consultas com mais de uma tabela.sql
@@ -0,0 +1,26 @@
+-- EXERCICIOS AULA 08
+/*
+01 - O que é JOIN? Quando é necessário?
+
+O JOIN é usado para cominar os dados de várias tabelas com base eu seus valores comuns.
+O JOIN é necessario quando existe tabelas diferentes com informacoes que queremos aprensentar de uma unica vez, para tal, tais tabelas
+precisando de um vinculo em comum.
+
+02 - Qual a sintaxe do JOIN? Maiúscula ou minúscula faz diferença? Existe algum padrão? Explique.
+
+Como dito no curso, a sintaxe do JOIN nao é definida dentro do SQL. Nao há diferença entre escrever com letras maiusculas ou minusculas, mas,
+para se manter um padrão, a comunidade busca escrever seguindo certas regras (varia de acordo com a empresa ou professor).
+
+04 - O que é primordial para que o resultado tenha sentido em consultas com JOIN? Explique.
+
+É primordial que o comando SELECT seja feito de maneira correta, pois é por meio dele que os dados sao "aglomerados" e exibidos corretamente.
+
+06 - Para realizar o JOIN de 1523 tabelas, quantas comparações de junções são necessárias? Explique.
+
+Para realizar o join de 1523, seriam feitas 1522 junções. A quantidade de comandos é porporcional a "n-1", onde por exemplo, utilizando
+um total de 10 tabelas, teriamos 9 junções.
+
+Exemplo: Tabela1 JUNÇÃO Tabela2 JUNÇÃO Tabela3 JUNÇÃO Tabela4 JUNÇÃO Tabela5 JUNÇÃO Tabela6 JUNÇÃO Tabela7 JUNÇÃO Tabela8 JUNÇÃO Tabela9 JUNÇÃO Tabela10
+*/
+
+ 
